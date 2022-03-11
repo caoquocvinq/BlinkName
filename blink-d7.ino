c5f5c743b5e5f56efe653826a08b9823d3c78d57
cqv273 <55745477+caoquocvinq@users.noreply.github.com>
2022-03-11 22:30:50 +1100

    Update blink-d7.ino

--- blink-d7.ino
+++ blink-d7.ino
@@ -5,16 +5,16 @@ SYSTEM_THREAD(ENABLED);
 void setup() {
 	pinMode(led1, OUTPUT);
 }
-
+// dot function turns the LED on for 1 sec and off for 1 sec.
 void dot() {
-    digitalWrite(led1, HIGH);
+	digitalWrite(led1, HIGH);
 	delay(1s);
 	digitalWrite(led1, LOW);
 	delay(1s);
 }
-
+// dash function turns the LED on for 2 secs and off for 1 sec.
 void dash() {
-    digitalWrite(led1, HIGH);
+    	digitalWrite(led1, HIGH);
 	delay(2s);
 	digitalWrite(led1, LOW);
 	delay(1s);
